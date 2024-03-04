d86cdb4679d935dd0bdaa1227c0123c550586565
enesozmus <ensozmus@gmail.com>
2024-03-04 15:30:26 +0300

    Initial Commit

---
+++ CustomViewModifiers/CustomViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifiersApp.swift
+//  CustomViewModifiers
+//
+//  Created by enesozmus on 4.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
